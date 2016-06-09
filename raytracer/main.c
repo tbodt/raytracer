f6f9f53560ec79bf54ea0177a72de4c0233a1a09
Theodore Dubois <tblodt@icloud.com>
2016-06-08 17:03:54 -0700

    Initial Commit

---
+++ raytracer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  raytracer
+//
+//  Created by Theodore Dubois on 6/8/16.
+//  Copyright © 2016 Theodore Dubois. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
